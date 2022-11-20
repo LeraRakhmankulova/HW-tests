ba6e5ee53227399f82508bdf2f4ab88307b47803
LeraRakhmankulova <lerakhmankulova@mail.ru>
2022-11-20 14:03:34 +0300

    generator

---
+++ Release/Program.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Xml.Serialization;
+
+namespace SeleniumTests
+{
+    class Program
+    {
+        private static string pathData = @"C:\Users\Valeria\Desktop\HW-tests\TestProject1\Models\";
+
+        //2 comment.xml xml
+        static void Main(string[] args)
+        {
+            var data = Console.ReadLine()?.Split();
+            if (data == null) return;
+            var dataCount = Convert.ToInt32(data[0]);
+            var fileName = pathData + data[1];
+            string dataFormat = data[2];
+            GenerateComments(dataCount, fileName, dataFormat);
+        }
+
+        static void GenerateComments(int count, string filename, string format)
+        {
+            var comments = new List<string>();
+            for (var i = 0; i < count; i++)
+            {
+                comments.Add(TestBase.GenerateRandomString(10));
+            }
+
+            StreamWriter writer = new StreamWriter(filename);
+            if (format == "xml")
+                WriteToXmlFile(comments, writer);
+            else
+                Console.Out.Write("Unrecognized format" + format);
+
+            writer.Close();
+        }
+
+        static void WriteToXmlFile(List<string> groups, TextWriter writer)
+        {
+            new XmlSerializer(typeof(List<string>)).Serialize(writer, groups);
+        }
+    }    
+}
--- TestProject1/Tests/TestBase.cs
+++ TestProject1/Tests/TestBase.cs
@@ -1,4 +1,5 @@
 ﻿using SeleniumTests;
+using System.Text;
 
 namespace SeleniumTests
 {
@@ -6,11 +7,25 @@ namespace SeleniumTests
     {
 
         protected AppManager app;
+        public static Random rnd = new Random();
 
         [SetUp]
         public void SetupTest()
         {
             app = AppManager.GetInstance();
         }
+        public static string GenerateRandomString(int max)
+        {
+            int rndValue = Convert.ToInt32(rnd.NextDouble() * max);
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < rndValue; i++)
+            {
+                builder.Append(Convert.ToChar(32 + Convert.ToInt32(rnd.NextDouble() * 65)));
+
+            }
+            return builder.ToString();
+        }
+
     }
 }
