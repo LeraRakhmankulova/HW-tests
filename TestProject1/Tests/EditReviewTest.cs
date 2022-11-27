48643716b480676c3728e90cd1932155816c6529
LeraRakhmankulova <lerakhmankulova@mail.ru>
2022-11-27 12:09:45 +0300

    6

--- TestProject1/Tests/EditReviewTest.cs
+++ TestProject1/Tests/EditReviewTest.cs
@@ -4,20 +4,22 @@ namespace SeleniumTests
 
 {
     [TestFixture]
-    public class EditReviewTest : TestBase
+    public class EditReviewTest : AuthBase
     {
 
-        [Test, Order(4)]
+        [Test, Order(2)]
         public void EditReviewhCaseTest()
         {
-            app.Navigation.OpenForumnPage();
 
-            CommentData comment = new CommentData("как дела?");
+            app.Navigation.OpenForumnPage();
+            CommentData comment = new CommentData("наверное нет...");
             app.Comment.CreateNewComment(comment);
 
-            CommentData editedComment = new CommentData("привет!");
+            CommentData editedComment = new CommentData("наверное нет... жаль");
             app.Comment.EditComment(editedComment);
+
+            CommentData newComment = app.Comment.GetCreatedComment();
+            Assert.That(newComment.Description, Is.EqualTo(comment.Description));
         }
     }
 }
-
