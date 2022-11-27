685760baf87d65be699aa7df0fa77ad1c5394b05
LeraRakhmankulova <lerakhmankulova@mail.ru>
2022-11-27 11:44:54 +0300

    done

--- TestProject1/Tests/CommentCreationTest.cs
+++ TestProject1/Tests/CommentCreationTest.cs
@@ -28,18 +28,6 @@ namespace SeleniumTests
             CommentData newComment = app.Comment.GetCreatedComment();
             Assert.That(newComment.Description, Is.EqualTo(comment.Description));
         }
-        public static IEnumerable<CommentData> CommentDataFromXmlFile()
-        {
-            return (List<CommentData>)new XmlSerializer(typeof(List<CommentData>))
-                .Deserialize(new StreamReader(@"..\..\..\Data\comments.xml"));
-        }
-
-        [Test, Order(3), TestCaseSource(nameof(CommentDataFromXmlFile))]
-        public void CreateFromXml(CommentData commentData)
-        {
-            app.Navigation.OpenForumnPage();
-            app.Comment.CreateNewComment(commentData);
-        }
     }
 }
 
---
+++ TestProject1/Tests/LoginTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class LoginTest : TestBase
+    {
+        [Test, Order(1)]
+        public void LoginCaseTest()
+        {
+            app.Navigation.OpenHomePage();
+            AccountData user = new AccountData("test1000lera@mail.ru", "Le26ra1703.");
+            app.Auth.Login(user);
+            app.Navigation.OpenHomePage();
+        }
+    }
+}
